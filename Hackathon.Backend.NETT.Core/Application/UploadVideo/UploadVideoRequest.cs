e8e58a3ea4705e2dfa9153140a380c85c96a9202
douglas emerick <dgrippemerick@gmail.com>
2024-03-16 13:05:22 -0300

    correçao

--- Hackathon.Backend.NETT.Core/Application/UploadVideo/UploadVideoRequest.cs
+++ Hackathon.Backend.NETT.Core/Application/UploadVideo/UploadVideoRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hackathon.Backend.NETT.Core.Application.UploadVideo
 {
-    public class UploadVideoRequest()
+    public class UploadVideoRequest
     {
     }
 }
