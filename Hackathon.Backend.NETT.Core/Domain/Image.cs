d6242ef767182edb8d16a4a01f7192dfb4db6530
douglas emerick <dgrippemerick@gmail.com>
2024-03-16 12:57:17 -0300

    Model

---
+++ Hackathon.Backend.NETT.Core/Domain/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Backend.NETT.Core.Domain
+{
+    public class Image
+    {
+        public Guid ImageId { get; set; }
+        public Guid VideoId { get; set; }
+        public string NameImage { get; set; }
+        public string PathImage { get; set; }
+        public DateTime CreateAt { get; private set; }
+    }
+}
